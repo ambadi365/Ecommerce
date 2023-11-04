9638399e29fa4656ebf418a6cb9adf71c807f965
ambadi123 <ambadi@dataguardnxt.com>
2023-11-04 22:55:14 +0530

    removed proxy.conf.json

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,21 +1,21 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { IProduct } from '../pages/home/product.interface';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { IProduct } from "../pages/home/product.interface";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ProductService {
-baseUrl:string = "/api/Ecomm"
-  constructor(private http:HttpClient) { }
+  baseUrl: string = "https://onlinetestapi.gerasim.in/api/Ecomm";
 
+  constructor(private http: HttpClient) {}
 
-  getAllProducts():Observable<IProduct> {
-    return this.http.get<IProduct>(`${this.baseUrl}/GetAllProducts`)
+  getAllProducts(): Observable<IProduct> {
+    return this.http.get<IProduct>(`${this.baseUrl}/GetAllProducts`);
   }
 
-  addToCart(body: any):Observable<any> {
-return this.http.post<any>(`${this.baseUrl}/AddToCart`,body)
+  addToCart(body: any): Observable<any> {
+    return this.http.post<any>(`${this.baseUrl}/AddToCart`, body);
   }
 }
